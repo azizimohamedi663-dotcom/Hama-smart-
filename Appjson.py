feeb3d98802057dddb2aed537237a66a73e078e1
azizimohamedi663-dotcom <azizimohamedi663@gmail.com>
2026-09-24 15:31:07 +0300

    Create Appjson.py

---
+++ Appjson.py
@@ -0,0 +1,15 @@
+{
+  "name": "HAMA SMART APP",
+  "short_name": "HAMA",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#FF6B00",
+  "theme_color": "#FF6B00",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3774/3774080.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
